fc2e73c3d97fd8711d33388bd274c869c0fb5f72
okech-michael <okechonyango365@gmail.com>
2026-05-24 11:46:49 +0300

    Fix: Double logo size to 160px

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -33,15 +33,15 @@ export default function Navbar() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-24 lg:h-28">
+        <div className="flex items-center justify-between h-40 lg:h-48">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2.5 group">
-            <div className="relative w-20 h-20">
+            <div className="relative w-40 h-40">
               <Image
                 src="/logo.png"
                 alt="E-health Logo"
-                width={80}
-                height={80}
+                width={160}
+                height={160}
                 className="object-contain"
               />
             </div>
